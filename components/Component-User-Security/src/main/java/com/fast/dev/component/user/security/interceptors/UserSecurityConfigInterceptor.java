35f7ae8a95212b666a4dba21fa73c346dc26656e
lianshufeng <251708339@qq.com>
2017-11-03 21:28:29 +0800

    更换 用户安全模块的路径

---
+++ components/Component-User-Security/src/main/java/com/fast/dev/component/user/security/interceptors/UserSecurityConfigInterceptor.java
@@ -0,0 +1,69 @@
+package com.fast.dev.component.user.security.interceptors;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+
+import com.fast.dev.component.user.security.model.UserSecurityConfig;
+import com.fast.dev.component.user.security.service.impl.UserSecurityAuthenticationManager;
+import com.fast.dev.core.interceptor.UrlInterceptor;
+
+/**
+ * 权限需要访问方法的拦截器
+ * 
+ * @作者 练书锋
+ * @联系 251708339@qq.com
+ * @时间 2017年11月1日
+ *
+ */
+public class UserSecurityConfigInterceptor implements UrlInterceptor {
+
+	private UserSecurityConfig userSecurityConfig;
+
+	public UserSecurityConfigInterceptor() {
+	}
+
+	public UserSecurityConfigInterceptor(UserSecurityConfig userSecurityConfig) {
+		super();
+		this.userSecurityConfig = userSecurityConfig;
+	}
+
+	@Resource
+	private UserSecurityAuthenticationManager userSecurityAuthenticationManager;
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		userSecurityAuthenticationManager.preHandle(request);
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		userSecurityAuthenticationManager.afterCompletion(request);
+	}
+
+	@Override
+	public String[] addPathPatterns() {
+		return userSecurityConfig.getNeedSecurityMethodUrl();
+	}
+
+	@Override
+	public String[] excludePathPatterns() {
+		return userSecurityConfig.getExcludeSecurityMethodUrl();
+	}
+
+	@Override
+	public int level() {
+		return 0;
+	}
+
+}
--- examples/Example-UserSecurity/src/main/java/com/fast/dev/example/security/controller/UserController.java
+++ examples/Example-UserSecurity/src/main/java/com/fast/dev/example/security/controller/UserController.java
@@ -5,10 +5,10 @@ import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.fast.dev.component.user.security.service.UserSecurityHelper;
 import com.fast.dev.core.model.InvokerResult;
 import com.fast.dev.core.util.code.JsonUtil;
 import com.fast.dev.example.security.model.UserInfo;
-import com.fast.dev.user.security.service.UserSecurityHelper;
 
 @Controller
 @RequestMapping("user")
