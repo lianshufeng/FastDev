6bfaab5b83ac489470cc490f332d3c72274dd156
188140040@qq.com <188140040@qq.com>
2017-11-13 14:58:41 +0800

    更换代码位置

---
+++ components/Component-yunpian-sms/src/main/java/com/fast/dev/component/yunpian/sms/utils/TplHelper.java
@@ -0,0 +1,62 @@
+package com.fast.dev.component.yunpian.sms.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Map;
+
+public class TplHelper {
+	
+  public static String ConvertTpl(Map<String, String>map) throws UnsupportedEncodingException{
+	  
+	  String tpl_value="";
+	  
+	  int i =0;
+	  
+	  for (String key : map.keySet()) {
+		  //在此处添加Key和Value 参数
+		 
+		  if(i ==map.size()-1 || map.size() == 1){
+			  //结束处不需要&
+			  tpl_value = tpl_value +URLEncoder.encode("#"+key+"#", "UTF-8")+"="+ URLEncoder.encode(map.get(key), "UTF-8");
+		  }else {
+			  tpl_value =tpl_value + URLEncoder.encode("#"+key+"#", "UTF-8")+"="+ URLEncoder.encode(map.get(key), "UTF-8") + "&";
+		  }
+		  
+		  
+		  
+		  i = i + 1;
+	 }
+		
+	  
+	return tpl_value;
+	  
+	  
+  }
+  /**
+   * 将数字转换文字
+   * @return
+   */
+ public static String ConvertNumToChar(String num){
+	 
+	 int length = num.length();
+	 
+	 String returnChar = "";
+	 
+	 for(int i=1;i<=length;i++){
+		 
+		 returnChar = returnChar +getCharByNum(Integer.parseInt(num.substring(i-1,i)));
+		 
+	 }
+	 
+	 return returnChar;
+ }
+  
+ public static String getCharByNum(int num){
+	 
+	  char[] numArray = { '零', '一', '二', '三', '四', '五', '六', '七', '八', '九' };
+	 
+	 
+	 return String.valueOf(numArray[num]);
+ }
+ 
+}
