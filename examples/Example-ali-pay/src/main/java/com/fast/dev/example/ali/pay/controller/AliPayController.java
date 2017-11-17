71b272a5059487c9c0401c22ebe6f30d5660cf9b
188140040@qq.com <188140040@qq.com>
2017-11-17 12:58:14 +0800

    支付接口新增APP 支付

--- examples/Example-ali-pay/src/main/java/com/fast/dev/example/ali/pay/controller/AliPayController.java
+++ examples/Example-ali-pay/src/main/java/com/fast/dev/example/ali/pay/controller/AliPayController.java
@@ -1,6 +1,9 @@
 package com.fast.dev.example.ali.pay.controller;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -8,11 +11,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.alipay.api.request.AlipayFundTransToaccountTransferRequest;
+import com.alibaba.fastjson.JSON;
+import com.alipay.api.AlipayApiException;
+import com.alipay.api.internal.util.AlipaySignature;
 import com.alipay.api.response.AlipayFundTransToaccountTransferResponse;
+import com.alipay.api.response.AlipayTradeAppPayResponse;
 import com.alipay.api.response.AlipayTradeQueryResponse;
+import com.fast.dev.component.ali.pay.model.AliPayConfig;
 import com.fast.dev.component.ali.pay.model.AliPayOrder;
+import com.fast.dev.component.ali.pay.model.BizContent;
 import com.fast.dev.component.ali.pay.service.AliPayService;
+import com.fast.dev.component.ali.pay.util.OrderInfoUtil;
 import com.fast.dev.core.model.InvokerResult;
 
 @Controller
@@ -22,6 +31,38 @@ public class AliPayController {
 	@Autowired
 	private AliPayService aliPayService;
 	
+	@Autowired
+	private AliPayConfig aliPayConfig;
+	
+			@RequestMapping("appPay.json")
+			public InvokerResult<String> appPay(HttpServletResponse httpResponse,HttpServletResponse resp,String out_trade_no,String subject,String total_amount,String body,String timeout_express) throws AlipayApiException, UnsupportedEncodingException{
+				//将参数填写进请求表单
+				
+				//内容
+				body="测试支付";
+				//订单号
+				out_trade_no = "2017"+System.currentTimeMillis();
+				//主题
+				subject = "订单支付";
+				//金额
+				total_amount="0.01";
+						
+				AliPayOrder aliPayOrder = new AliPayOrder();
+				
+				aliPayOrder.setBody(body);
+				
+				aliPayOrder.setOut_trade_no(out_trade_no);
+				
+				aliPayOrder.setSubject(subject);
+				
+				aliPayOrder.setTotal_amount(total_amount);
+				
+				String orderInfo = aliPayService.appPay(aliPayOrder);
+				
+				return new InvokerResult<>(orderInfo);
+			}
+			
+			
 			@RequestMapping("pay.json")
 			public InvokerResult<String> pay(HttpServletResponse httpResponse,HttpServletResponse resp,String out_trade_no,String subject,String total_amount,String body,String timeout_express,String product_code) throws IOException{
 
@@ -68,5 +109,8 @@ public class AliPayController {
 				
 				return new InvokerResult<>(response);
 			}
+			
+			
+			
 			
 }
