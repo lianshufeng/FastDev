b3e2b099ab361ea298747c8195c4414dcb67644b
188140040@qq.com <188140040@qq.com>
2017-12-01 11:28:37 +0800

    新增流输入

--- components/Component-ucloud-file/src/main/java/com/fast/dev/component/ucloud/file/service/FileService.java
+++ components/Component-ucloud-file/src/main/java/com/fast/dev/component/ucloud/file/service/FileService.java
@@ -1,8 +1,13 @@
 package com.fast.dev.component.ucloud.file.service;
 
+import java.io.FileInputStream;
+
 public interface FileService {
 
 	//上传文件
 	public String uploadFile(String key,String filePath);
+	
+	//上传文件
+	public String uploadFile(String key,FileInputStream fileInputStream);
 	
 }
--- components/Component-ucloud-file/src/main/java/com/fast/dev/component/ucloud/file/service/impl/FileServiceImpl.java
+++ components/Component-ucloud-file/src/main/java/com/fast/dev/component/ucloud/file/service/impl/FileServiceImpl.java
@@ -2,6 +2,8 @@ package com.fast.dev.component.ucloud.file.service.impl;
 
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -21,6 +23,7 @@ import cn.ucloud.ufile.UFileRequest;
 import cn.ucloud.ufile.UFileResponse;
 import cn.ucloud.ufile.sender.GetSender;
 import cn.ucloud.ufile.sender.PostSender;
+import cn.ucloud.ufile.sender.PutSender;
 @Component
 public class FileServiceImpl implements FileService{
 
@@ -73,117 +76,68 @@ public class FileServiceImpl implements FileService{
 		
 		UFileResponse response = sender.send(ufileClient, request);
 		if (response != null) {
-			
-			//System.out.println("status line: " + response.getStatusLine());
-			
-			Header[] headers = response.getHeaders();
-			for (int i = 0; i < headers.length; i++) {
-				//System.out.println("header " + headers[i].getName() + " : " + headers[i].getValue());
+			if(response.getStatusLine().getStatusCode() ==200){
+				return true;
 			}
-			
-			//System.out.println("body length: " + response.getContentLength());
-			
-			InputStream inputStream = response.getContent();
-			if (inputStream != null) {
-				try {
-					BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-					String s = "";
-					while ((s = reader.readLine()) != null) {
-						System.out.println(s);
-					}		
-				} catch (IOException e) {
-					e.printStackTrace();
-				} finally {
-					if (inputStream != null) {
-						try {
-							inputStream.close();
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-					}
-				}
-			}
-		}
-		if(response.getStatusLine().getStatusCode() ==200){
-			return true;
 		}
 		return false;
 	}
+
+	@Override
+	public String uploadFile(String key, FileInputStream fileInputStream) {
+		boolean isUpload=false;
+		
+		String bucketName = uCloudFileConfig.getBucketName();
+		int i = 0;
+		
+		if(System.getProperty("os.name").toLowerCase().startsWith("win")) {
+			i =1;
+		}
+		
+		String configPath =this.getClass().getClassLoader().getResource("/").getPath().substring(i) +"config.properties";
+		
+		UFileConfig.getInstance().loadConfig(configPath);
+		
+		UFileRequest request = new UFileRequest();
+		request.setBucketName(bucketName);
+		request.setKey(key);
+		
+		
+		try {
+			request.setInputStream(fileInputStream);
+			request.setContentLength(fileInputStream.available());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		UFileClient ufileClient = null;
+		
+		try {
+			ufileClient = new UFileClient();
+			isUpload = putFile(ufileClient, request);
+		} finally {
+			ufileClient.shutdown();
+		}
+		
+		if(isUpload){
+			 return "http://"+bucketName+".ufile.ucloud.com.cn/"+key;
+		}
+		
+		return "";
+	}
 	
-	public  boolean getFile(UFileClient ufileClient, UFileRequest request, String saveAsPath) {
-		GetSender sender = new GetSender();
+	
+	private  boolean putFile(UFileClient ufileClient, UFileRequest request) {
+		PutSender sender = new PutSender();
 		sender.makeAuth(ufileClient, request);
 		
 		UFileResponse response = sender.send(ufileClient, request);
-
 		if (response != null) {
 			
-			//System.out.println("status line: " + response.getStatusLine());
-		
-			Header[] headers = response.getHeaders();
-			for (int i = 0; i < headers.length; i++) {
-				System.out.println("header " + headers[i].getName() + " : " + headers[i].getValue());
+			if(response.getStatusLine().getStatusCode()==200){
+				return true;
 			}
-		
-			//System.out.println("body length: " + response.getContentLength());
-			
-			//handler error response 	
-			if (response.getStatusLine().getStatusCode() != 200 && response.getContent() != null) {
-				BufferedReader br = null;
-				try {
-					br = new BufferedReader(new InputStreamReader(response.getContent()));
-					String input;
-					while((input = br.readLine()) != null) {
-						//System.out.println(input);
-					}
-				} catch (IOException e) {
-					e.printStackTrace();
-				} finally {
-					if (response.getContent() != null) {
-						try {
-							response.getContent().close();
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-					}
-				}
-			} else {
-				InputStream inputStream = null;
-				OutputStream outputStream = null;
-				try {
-					inputStream = response.getContent();
-					outputStream = new BufferedOutputStream(new FileOutputStream(saveAsPath));
-			        int bufSize = 1024 * 4;
-			        byte[] buffer = new byte[bufSize];
-			        int bytesRead;
-			        while ((bytesRead = inputStream.read(buffer)) > 0) {
-			        	outputStream.write(buffer, 0, bytesRead);
-			        }
-					
-				} catch (IOException e) {
-					e.printStackTrace();
-				} finally {
-					if (inputStream != null) {
-						try {
-							inputStream.close();
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-					}
-					if (outputStream != null) {
-						try {
-							outputStream.close();
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-					}
-				}
-			}
-		}
-		
-		if(response.getStatusLine().getStatusCode() != 200){
-			
-			return true;
 			
 		}
 		return false;
